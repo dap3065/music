876919f530c0d26b25aa127fc6030600255b4f6b
OpenShift System User <54d1c1025973ca7375000046@ex-std-node521.prod.rhcloud.com>
2015-04-15 16:34:51 -0400

    First commit of music site

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OG PROFIT K</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/jquery.bxslider.css" type="text/css" />
+    <!-- Custom CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i>  <span class="light">Start</span> Music
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#music">Music</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#videos">Videos</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#pics">Pics</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#download">Download</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">OG PROFIT K</h1>
+                        <p class="intro-text">Taking over the rap game.<br>Created by David.</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>About OG Profit K</h2>
+                <p>Fort Worth, TX on the scene. Get music, it can be yours right now, simply download the <a href="#download">here</a>. The music is now on sale for a limited time.</p>
+                <p>This website features stock photos by <a href="#music">Villian Music</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
+                <p>Contact David for easy customization to any website solution.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Music Section -->
+    <section id="music" class="content-section text-center">
+        <div class="music-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>OG Profit K Music Player</h2>
+                    <p>Stream the top hits now!</p>
+                    <iframe width="100%" height="170" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/66392700&amp;color=f2ab00&amp;theme_color=000000&amp;auto_play=false&amp;hide_related=true&amp;show_artwork=false"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Videos Section -->
+    <section id="videos" class="content-section text-center">
+        <div class="videos-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>OG Profit K Music Videos</h2>
+                    <p>Watch your favorite videos here!</p>
+                    <p>
+                    <ul id="slider1">
+                    <li>
+                    <iframe  width="100%" height="100%" src="https://www.youtube.com/embed/dTOYL1CzVgY" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>
+                    </li>
+  <li>
+<iframe width="100%" height="100%" src="http://www.youtube.com/embed/DJFm-3G87nc" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+  </li>
+  <li>
+  <iframe width="100%" height="100%" src="http://www.youtube.com/embed/HEU2WtbG-6M" frameborder="0" allowfullscreen></iframe>                
+  
+  </li>
+</ul>
+
+</p>
+<p>
+	</p>
+				</div>
+            </div>
+        </div>
+    </section>
+    <!-- Pics Section -->
+    <section id="pics" class="content-section text-center">
+        <div class="pics-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>OG Profit K Latest Pictures</h2>
+                    <p>Stay up to date!</p>
+                    <p><ul id="slider2">
+  <li><img src="img/Bless_edit.jpg" /></li>
+  <li><p>More Pics Coming Soon</p></li>
+  <li><img src="img/Bless_edit.jpg" /></li>
+</ul></p>
+				</div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Download Section -->
+    <section id="download" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Download OG Profit K Music</h2>
+                    <p>You can download music now!</p>
+                    <a href="#download" class="btn btn-default btn-lg">Visit Download Page</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact OG Profit K</h2>
+                <p>Feel free to email me to provide some feedback on my site, give us suggestions for new music, or to just say hello!</p>
+                <p><a href="mailto:dap3065@yahoo.com">dap3065@yahoo.com</a>
+                </p>
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/dap3065/" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <div id="map"></div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; OG Profit K 2015</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+  	<script src="plugins/jquery.fitvids.js"></script>
+  	<script src="js/jquery.bxslider.js"></script>
+  	<script type="text/javascript">
+  	
+  	$('#slider1').bxSlider({
+  	  mode: 'fade',
+  	  video: true,
+  	  autoControls: true,
+  	  useCSS: false
+  	});
+
+  	$('#slider2').bxSlider({
+  	  auto: true,
+  	  autoControls: true,
+  	  pause: 3000,
+  	  slideMargin: 20
+  	});  	</script>
+    
+</body>
+
+</html>
